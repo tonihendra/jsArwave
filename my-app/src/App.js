e123e2adee555c76f1d415793b730ae2143a6e0d
Toni Hendra <43691489+tonihendra@users.noreply.github.com>
2024-12-29 07:12:22 +0000

    feat: initiated the project

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <HashRouter>
+      <div>
+        {/* Your routes and components go here */}
+        <h1>Welcome to Dencuan HashRouter</h1>
+      </div>
+    </HashRouter>
+  );
+}
